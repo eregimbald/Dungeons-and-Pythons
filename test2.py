e90f4fc4d99b54f848cfe7dea7cac237583d5314
eregimbald <eregimbald@fuze.com>
2017-05-26 15:44:32 -0400

    Delete test2.py

--- test2.py
+++
@@ -1,6 +0,0 @@
-import math
-
-time = 150
-
-
-print "With {0} soldiers moving abreast, the passage took [{1} minutes, {2} seconds] or [{3} rounds] to cross.".format(15, time * 60 // 60, time * 60 % 60, math.ceil(time * 6))
